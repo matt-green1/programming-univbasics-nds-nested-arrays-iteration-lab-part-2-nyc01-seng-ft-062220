7cd7f0722ca71755db406a0b0199f90238cc9b92
Matt Green <magreen118@gmail.com>
2020-05-31 23:58:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,10 +7,10 @@ def find_min_in_nested_arrays(src)
     while element_count < src[row_index].count do
       if src[row_index][element_count] < lowest_int
         lowest_int = src[row_index][element_count]
-        weekly_minimums.push(lowest_int)
       end
     element_count += 1
     end
+    weekly_minimums.push(lowest_int)
     row_index += 1
   end
   return weekly_minimums
